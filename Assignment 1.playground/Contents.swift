59d74f7f0e7b77a07cd2544246b64f24345fb286
Brandon South <Brandon.south@smail.Astate.edu>
2020-08-29 22:15:15 -0400

    Assignment 1 "bio"

---
+++ Assignment 1.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+var bio = """
+Name: Brandon South
+Age: 23
+Birthplace: Forrest City, Arkansas
+My career asipirations are to become a 3-D artist / modeler and work for a tech firm near the beach.
+"""
+
+
+
+
