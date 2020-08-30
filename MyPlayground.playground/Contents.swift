84bc0e4795afd6841726bc9359c1bfa60d83d716
Brandon South <Brandon.south@smail.Astate.edu>
2020-08-29 21:12:09 -0400

    Adding my playground file to GitHub

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var integer = 10
+
+var string2 = "Strings always esist in quotes"
+
+var integer2 = 20
+
+str
+integer
+integer2
+integer + integer2
+integer * integer2
+integer2 / integer
+
+str + string2
+
+str + String(integer)
